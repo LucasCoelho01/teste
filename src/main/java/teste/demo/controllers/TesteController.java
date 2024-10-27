896ae858c4b55281504d87c4e38695ba3cc8f678
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2024-10-27 17:00:29 -0300

    first commit

---
+++ src/main/java/teste/demo/controllers/TesteController.java
@@ -0,0 +1,18 @@
+package teste.demo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("teste")
+public class TesteController {
+
+    @GetMapping
+    public ResponseEntity<String> teste() {
+        return new ResponseEntity<>("Teste", HttpStatus.OK);
+    }
+}
